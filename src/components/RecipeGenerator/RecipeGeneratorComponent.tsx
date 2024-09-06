1537cebc22cef4a630bf65d8950ce99ec416212a
Levon <zhaoxiaofengpy@gmail.com>
2024-09-06 15:27:36 -0700

    fix import react issue

--- src/components/RecipeGenerator/RecipeGeneratorComponent.tsx
+++ src/components/RecipeGenerator/RecipeGeneratorComponent.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 import "../../styles/index.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { FormEvent, useState } from "react";
