4d899fb1c5a4905788dfc16ec57a87733b441de3
Levon <zhaoxiaofengpy@gmail.com>
2024-11-26 16:26:46 -0800

    generate functionality merge

--- src/services/pathFindingService.ts
+++ src/services/pathFindingService.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://[your-api-id].execute-api.[your-region].amazonaws.com/[stage]';
+const BASE_URL = 'https://ozqm91hsm0.execute-api.us-east-1.amazonaws.com/pathfinding-generator';
 
 export const pathFindingService = {
   generateNewMap: async (algorithm: number, obstacleCount: number) => {
     try {
-      const response = await axios.post(`${BASE_URL}/generate-map`, { 
-        algorithm,
-        obstacleCount,
-      });
+      const response = await axios.post(BASE_URL, 
+        { 
+          algorithm,
+          obstacleCount,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+      );
       return response.data;
     } catch (error) {
       console.error('Error generating map:', error);
